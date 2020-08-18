9d2354c9d8035670664a96ea7042eb6a8135dceb
Jamie <jrk1443@rit.edu>
2020-08-17 20:16:33 -0400

    Modified main to test comms with mag/accel unit

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include "MK64F12.h"
 #include "uart_drvr.h"
 #include "gpio_drvr.h"
+#include "mag_accel_drvr.h"
+#include "i2c_drvr.h"
 #include <string.h>
 #include <math.h>
 
@@ -20,28 +22,47 @@ int main(void)
 {  
 	// Initialize UART
 	initialize();
-	Blue_LED(LED_ON);
+  //Blue_LED(LED_ON);
+  if(FXOS8700CQ_init() == 1)
+  {
+    put("I2C initialization failure! :(\n\r");
+    Red_LED(LED_ON);
+  }
+  else
+  {
+	  put("I2C initialization success! :)\n\r");
+    Green_LED(LED_ON);
+  }
   
   for(;;)
   {
-    if(SW2_pressed())
+    if(SW3_pressed())
     {
-      Blue_LED(LED_TOGGLE);
-      Red_LED(LED_ON);
-      put("SW2 Pressed!\n\r");
+      //Blue_LED(LED_TOGGLE);
+      //Red_LED(LED_ON);
+      if(whoami() == 1)
+      {
+        put("I2C failure! :(\n\r");
+        Red_LED(LED_ON);
+      }
+      else
+      {
+	      put("I2C success! :)\n\r");
+        Green_LED(LED_ON);
+      }
       delay(50);
-      Red_LED(LED_OFF);
-      Blue_LED(LED_TOGGLE);
-    }
-    else if(SW3_pressed())
+      //Red_LED(LED_OFF);
+      //Blue_LED(LED_TOGGLE);
+    }/*
+    else if(SW2_pressed())
     {
       Blue_LED(LED_TOGGLE);
       Green_LED(LED_ON);
-      put("SW3 Pressed!\n\r");
+      put("SW2 Pressed!\n\r");
       delay(50);
       Green_LED(LED_OFF);
       Blue_LED(LED_TOGGLE);
-    }
+    }*/
   }
   return 0;
 }
@@ -63,6 +84,6 @@ void initialize()
 	// Initialize UART
 	uart_init();
   LED_init();
-  SW2_init();
+  //SW2_init();
   SW3_init();
 }
